c6baf94d4f3b9302f51779db5340089f5f2ce34c
t203008 <91816208+t203008@users.noreply.github.com>
2022-11-08 15:06:31 +0900

    Update data.py

--- data.py
+++ data.py
@@ -4,6 +4,8 @@ st.title("競馬")
 a=st.radio("データ選択", ("全レース", "レース賞別", "該当レース")) #第一引数：リスト名（選択肢群の上に表示）、第二引数：選択肢
 if a=="該当レース":
   y=st.selectbox("レース選択",("宝塚記念","スプリンターズS","秋華賞"))
+elif a=="レース賞別":
+  y=st.selectbox("レース賞選択",("G1","G2","G3"))
 
 horse_all=pd.read_csv("Horse_Race.csv")
 horse_all=horse_all.fillna(0)
