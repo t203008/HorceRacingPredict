09f499d6d6ae78b5ace4669c66f73d1ca4b577fc
t203008 <91816208+t203008@users.noreply.github.com>
2022-11-08 14:54:58 +0900

    Update data.py

--- data.py
+++ data.py
@@ -5,4 +5,8 @@ horse_all=pd.read_csv("Horse_Race.csv")
 horse_all=horse_all.fillna(0)
 horse_all["rank*class"]=horse_all["P_rank"]*horse_all["P_class-Class"]
 horse_all["pop*class"]=horse_all["P_popular"]*horse_all["P_class-Class"]
+st.write("全レースデータ")
 st.write(horse_all)
+st.write("G1レースデータ")
+horse_g1=horse_all[(all["Race_Grade"]==1)]
+st.write(horse_g1)
