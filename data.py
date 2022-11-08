93ae9901597de9d6b0706d113378acf2216d72ea
t203008 <91816208+t203008@users.noreply.github.com>
2022-11-08 14:50:02 +0900

    Update data.py

--- data.py
+++ data.py
@@ -1,4 +1,8 @@
 import streamlit as st
 import pandas as pd
+st.title("競馬")
 horse_all=pd.read_csv("Horse_Race.csv")
+horse_all=horse_all.fillna(0)
+horse_all["rank*class"]=horse_all["P_rank"]*horse_all["P_class-Class"]
+horse_all["pop*class"]=horse_all["P_popular"]*horse_all["P_class-Class"]
 st.write(horse_all)
