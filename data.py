2f35fc8a7e6d45d81e08b227a86454009f9250b2
t203008 <91816208+t203008@users.noreply.github.com>
2022-11-08 15:01:47 +0900

    Update data.py

--- data.py
+++ data.py
@@ -1,6 +1,9 @@
 import streamlit as st
 import pandas as pd
 st.title("競馬")
+a=st.radio("データ選択", ("全レース", "レース賞別", "該当レース")) #第一引数：リスト名（選択肢群の上に表示）、第二引数：選択肢
+if a=="該当レース":
+  y=st.selectbox("レース選択","宝塚記念")
 horse_all=pd.read_csv("Horse_Race.csv")
 horse_all=horse_all.fillna(0)
 horse_all["rank*class"]=horse_all["P_rank"]*horse_all["P_class-Class"]
