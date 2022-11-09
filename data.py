01029e5ea0ba01a921b7bcca77f26a07ef2f9326
t203008 <91816208+t203008@users.noreply.github.com>
2022-11-09 21:42:42 +0900

    Update data.py

--- data.py
+++ data.py
@@ -14,12 +14,12 @@ st.sidebar.markdown("下のdownloadをクリックしてSampleデータを入力
 st.sidebar.markdown(f"{href}", unsafe_allow_html=True)
 st.sidebar.markdown("入力例")
 image=Image.open("スクリーンショット (506).png")
+st.sidebar.image(image,caption="サイレンススズカの天皇賞(秋)に出る際の確率を調べたいとき",use_column_width=True)
+
 st.sidebar.markdown("")
 st.sidebar.markdown("### 2. ファイルアップロード")
 st.sidebar.markdown("1で作成したcsvファイルをドラッグ&ドロップしてください")
 uploaded_file = st.sidebar.file_uploader("CSVファイルをドラッグ&ドロップ", type='csv', key='train')
-
-st.sidebar.image(image,caption="サイレンススズカの天皇賞(秋)に出る際の確率を調べたいとき",use_column_width=True)
 
 a=st.radio("データ選択", ("全レース", "レース賞別", "該当レース")) #第一引数：リスト名（選択肢群の上に表示）、第二引数：選択肢
 
