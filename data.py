95e3e76967962629c5b6031e04506e7cfffd693f
t203008 <91816208+t203008@users.noreply.github.com>
2022-11-10 11:22:48 +0900

    Update data.py

--- data.py
+++ data.py
@@ -63,6 +63,9 @@ if pred is not None:
   dellist=["Race_Grade","Win","Quinella","Show","rank*class","pop*class"]
   for i in dellist:
     sub.remove(i)
-  st.markdown("sub")
-  st.dataframe(predict)
+  st.markdown(入力データの確認)
+  st.daftaframe(predict)
+  sub.insert(1,"Race")
+  predicte=pd.read_csv(pred,names=sub)
+  
 #  st.write(LR1.predict(predict))
