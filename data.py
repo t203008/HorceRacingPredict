8d5e805c4a8aab80f0028dac54ca9357134882f2
t203008 <91816208+t203008@users.noreply.github.com>
2022-11-10 11:41:03 +0900

    Update data.py

--- data.py
+++ data.py
@@ -17,12 +17,6 @@ st.sidebar.markdown("入力例")
 image=Image.open("スクリーンショット (506).png")
 st.sidebar.image(image,caption="サイレンススズカの天皇賞(秋)に出る際の確率を調べたいとき",use_column_width=True)
 
-st.sidebar.markdown("")
-st.sidebar.markdown("### 2. ファイルアップロード")
-st.sidebar.markdown("1で作成したcsvファイルをドラッグ&ドロップしてください")
-pred=st.sidebar.file_uploader("CSVファイルをドラッグ&ドロップ", type='csv', key='train')
-
-
 a=st.radio("データ選択", ("全レース", "レース賞別", "該当レース")) #第一引数：リスト名（選択肢群の上に表示）、第二引数：選択肢
 
 horse_all=pd.read_csv("Horse_Race.csv")
@@ -56,7 +50,13 @@ elif a=="レース賞別":
 
 st.write("未実装です")
 
+st.sidebar.markdown("")
+st.sidebar.markdown("### 2. ファイルアップロード")
+st.sidebar.markdown("1で作成したcsvファイルをドラッグ&ドロップしてください")
+pred=st.sidebar.file_uploader("CSVファイルをドラッグ&ドロップ", type='csv', key='train')
+
 if pred is not None:
+  pred1=pred
   predict=pd.read_csv(pred)
   predict=predict.fillna(0)
   sub=list(horse_all.columns.values)
@@ -66,8 +66,7 @@ if pred is not None:
   st.markdown("入力データの確認")
   st.dataframe(predict)
   sub.insert(0,"Horse")
-  st.write(sub)
-  predicte=pd.read_csv(pred,names=sub)
+  predicte=pd.read_csv(pred1,names=sub)
   st.write(predicte)
   
 #  st.write(LR1.predict(predict))
