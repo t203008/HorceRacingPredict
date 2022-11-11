d0ba624fc592909f45065b11b1dacff800e57e33
t203008 <91816208+t203008@users.noreply.github.com>
2022-11-11 15:05:22 +0900

    Update data.py

--- data.py
+++ data.py
@@ -102,7 +102,6 @@ if pred is not None:
   predic3=logistic3.predict(predic)
   pre=pd.concat([predic["Horse"],predic1,predic2,predic3],axis=1)
   pred=pre.rename(columns={"Horse":"馬名",0:"単勝率",1:"連対率",2:"複勝率"})
-  st.write(pred)
   L=["単勝率","連対率","複勝率"]
   K=1
   for i in L:
