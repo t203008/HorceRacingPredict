f717bc4ce114576e1af39b13d0873604831e090f
t203008 <91816208+t203008@users.noreply.github.com>
2022-11-11 11:53:37 +0900

    Update data.py

--- data.py
+++ data.py
@@ -67,13 +67,14 @@ if pred is not None:
   predict=pd.read_csv(pred,names=sub)
   predict=predict.fillna(0)
   predict=predict.drop(index=predict.index[[0]])
+  st.dataframe(predict)
   for i in sub:
     st.write(predict[i].dtype())
     predict[i]=predict[i].astype(float,errors="raise")
   predict["rank*class"]=predict["P_rank"]*predict["P_class-Class"]
   predict["pop*class"]=predict["P_popular"]*predict["P_class-Class"]
   st.markdown("入力データの確認")
-  st.dataframe(predict)
+
   
   logistic1 = smf.glm(formula = "Y1 ~ X",
                    data = Z ,
