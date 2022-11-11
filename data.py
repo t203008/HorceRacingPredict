33d86a5f3018b1458b4fe5eec850015c964b3b75
t203008 <91816208+t203008@users.noreply.github.com>
2022-11-11 12:24:58 +0900

    Update data.py

--- data.py
+++ data.py
@@ -67,7 +67,6 @@ if pred is not None:
   pred1=pred
   predic=pd.read_csv(pred)
   predic=predic.fillna(0)
-  st.write(predic)
   for i in sub:
     if i not in ["馬名","レース名"]:
       predic[i]=predic[i].astype(float,errors="raise")
