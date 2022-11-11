155ca77a7197d0b037424921d37c60df5178c7e5
t203008 <91816208+t203008@users.noreply.github.com>
2022-11-11 12:44:24 +0900

    Update data.py

--- data.py
+++ data.py
@@ -58,7 +58,6 @@ st.sidebar.markdown("1で作成したcsvファイルをドラッグ&ドロップ
 pred=st.sidebar.file_uploader("CSVファイルをドラッグ&ドロップ", type='csv', key='train')
 
 sub=list(horse_all.columns.values)
-st.write(sub)
 dellist=["Race_Grade","Win","Quinella","Show","rank_and_class","pop_and_class"]
 for i in dellist:
   sub.remove(i)
