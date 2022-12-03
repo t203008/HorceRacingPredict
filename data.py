623e7d1eadde2177530813bc6ba2c2b56304bdd8
t203008 <91816208+t203008@users.noreply.github.com>
2022-12-04 04:49:47 +0900

    Update data.py

--- data.py
+++ data.py
@@ -132,4 +132,6 @@ if pred is not None:
   st.markdown("3.3歳馬限定レースがあるため、3歳が評価されやすくなっています")
   st.markdown("4.該当レースから求めた確率はサンプル数が少なく、信ぴょう性に欠けます")
 
-  st.write(pred.T)
+  pred1=pred.drop["馬名"]
+  st.write(pred1)
+  st.write(pred)
