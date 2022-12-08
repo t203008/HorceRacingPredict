a06ea8c0d28d9361a804c7be8d407659a69ec882
t203008 <91816208+t203008@users.noreply.github.com>
2022-12-08 12:52:14 +0900

    Update data.py

--- data.py
+++ data.py
@@ -25,7 +25,7 @@ st.sidebar.markdown("2022年チャンピオンズカップ　データ")
 sample2=pd.read_csv("Champions Cup 2022.csv")
 sample2=sample2.to_csv(index=False)  
 b64 = base64.b64encode(sample2.encode('utf-8-sig')).decode()
-href = f'<a href="data:application/octet-stream;base64,{b64}" download="Champions Cup 2022.csv">Chanmpions Cup 2022</a>'
+href = f'<a href="data:application/octet-stream;base64,{b64}" download="Hanshin Juvenile Fillies 2022.csv">Hanshin Juvenile Fillies 2022</a>'
 st.sidebar.markdown(f"{href}", unsafe_allow_html=True)
 
 horse_all=pd.read_csv("Horse_Race.csv")
