1fee61acf1043bb8a03b9073963eeea670cc03f2
t203008 <91816208+t203008@users.noreply.github.com>
2022-12-18 02:04:32 +0900

    Update data.py

--- data.py
+++ data.py
@@ -124,13 +124,13 @@ if pred is not None:
   
   #st.dataframe(predic) 
   st.header("予測される確率") 
-  logistic1 = smf.glm(formula = "Win ~ 1+Age+two_age+Mare+Stallion+P_rank+P_popular+Jockey_change+Change_from_P_Grass+Change_from_P_Dirt+Change_from_P_Hurdle+P_class_Class+Weight_P_Weight+Distance_P_distance+Week_distance+two_Week+P_overseas+P_rank*P_popular*P_class_Class",
+  logistic1 = smf.glm(formula = "Win ~ 1+two_Weight+two_Distance+Age+two_age+Mare+Stallion+P_rank+P_popular+Jockey_change+Change_from_P_Grass+Change_from_P_Dirt+Change_from_P_Hurdle+P_class_Class+Weight_P_Weight+Distance_P_distance+Week_distance+two_Week+P_overseas+P_rank*P_popular*P_class_Class",
                    data = Z ,
                    family = sm.families.Binomial()).fit()
-  logistic2 = smf.glm(formula = "Quinella ~ 1+Age+two_age+Mare+Stallion+P_rank+P_popular+Jockey_change+Change_from_P_Grass+Change_from_P_Dirt+Change_from_P_Hurdle+P_class_Class+Weight_P_Weight+Distance_P_distance+Week_distance+two_Week+P_overseas+P_rank*P_popular*P_class_Class",
+  logistic2 = smf.glm(formula = "Quinella ~ 1+two_Weight+two_Distance+Age+two_age+Mare+Stallion+P_rank+P_popular+Jockey_change+Change_from_P_Grass+Change_from_P_Dirt+Change_from_P_Hurdle+P_class_Class+Weight_P_Weight+Distance_P_distance+Week_distance+two_Week+P_overseas+P_rank*P_popular*P_class_Class",
                    data = Z ,
                    family = sm.families.Binomial()).fit()
-  logistic3 = smf.glm(formula = "Show ~ 1+Age+two_age+Mare+Stallion+P_rank+P_popular+Jockey_change+Change_from_P_Grass+Change_from_P_Dirt+Change_from_P_Hurdle+P_class_Class+Weight_P_Weight+Distance_P_distance+Week_distance+two_Week+P_overseas+P_rank*P_popular*P_class_Class",
+  logistic3 = smf.glm(formula = "Show ~ 1+two_Weight+two_Distance+Age+two_age+Mare+Stallion+P_rank+P_popular+Jockey_change+Change_from_P_Grass+Change_from_P_Dirt+Change_from_P_Hurdle+P_class_Class+Weight_P_Weight+Distance_P_distance+Week_distance+two_Week+P_overseas+P_rank*P_popular*P_class_Class",
                    data = Z ,
                    family = sm.families.Binomial()).fit()
   
