699f09e293c92fc82080d17fa77dcf411ef6b52c
t203008 <91816208+t203008@users.noreply.github.com>
2023-04-09 13:52:09 +0900

    Update data.py

--- data.py
+++ data.py
@@ -153,6 +153,7 @@ if pred is not None:
                    data = Z ,
                    family = sm.families.Binomial()).fit()
     logistic3 = smf.glm(formula = "Show ~ 1+Age+Mare+Stallion+P_rank+P_popular+Jockey_change+Change_from_P_Grass+Change_from_P_Dirt+Change_from_P_Hurdle+P_class_Class+Weight_P_Weight+Distance_P_distance+Week_distance+P_overseas+P_rank*P_popular*P_class_Class",
+                   data=Z,
                    family = sm.families.Binomial()).fit()
   
   predic1=logistic1.predict(predic)
