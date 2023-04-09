7da95118e7aaa5da1818605c9c3b0df5dfbc9916
t203008 <91816208+t203008@users.noreply.github.com>
2023-04-09 12:37:23 +0900

    Update data.py

--- data.py
+++ data.py
@@ -54,7 +54,7 @@ a=st.radio("データ選択", ("全レース", "レース賞別", "該当レー�
 
 
 if a=="全レース":
-  X=horse_all.drop(["Race","Restrict","Mare limited","Race_Grade","Dirt","Distance","Course","Win","Quinella","Show"],axis=1)
+  X=horse_all.drop(["Race","Restrict","Mare Limited","Race_Grade","Dirt","Distance","Course","Win","Quinella","Show"],axis=1)
   Y1=horse_all["Win"]
   Y2=horse_all["Quinella"]
   Y3=horse_all["Show"]
@@ -84,7 +84,7 @@ elif a=="該当レース":
   elif y=="桜花賞":
     b="Oka Sho"
   this=horse_all[horse_all["Race"].str.contains(b)]
-  X=this.drop(["Race","Restrict","Mare limited","Race_Grade","Dirt","Distance","Course","Win","Quinella","Show"],axis=1)
+  X=this.drop(["Race","Restrict","Mare Limited","Race_Grade","Dirt","Distance","Course","Win","Quinella","Show"],axis=1)
   Y1=this["Win"]
   Y2=this["Quinella"]
   Y3=this["Show"]
@@ -93,7 +93,7 @@ elif a=="レース賞別":
   y=st.selectbox("レース賞選択",("G1","G2","G3"))
   y=int(y.replace("G",""))
   grade=horse_all[(horse_all["Race_Grade"]==y)]
-  X=grade.drop(["Race","Restrict","Mare limited","Race_Grade","Dirt","Distance","Course","Win","Quinella","Show"],axis=1)
+  X=grade.drop(["Race","Restrict","Mare Limited","Race_Grade","Dirt","Distance","Course","Win","Quinella","Show"],axis=1)
   Y1=grade["Win"]
   Y2=grade["Quinella"]
   Y3=grade["Show"]
@@ -109,7 +109,7 @@ pred=st.sidebar.file_uploader("CSVファイルをドラッグ&ドロップ", typ
 
 sub=list(horse_all.columns.values)
 st.write(sub)
-dellist=["Race_Grade","Mare limited","Restrict","Dirt","Distance","Course","Win","Quinella","Show","rank_and_class","pop_and_class"]
+dellist=["Race_Grade","Mare Limited","Restrict","Dirt","Distance","Course","Win","Quinella","Show","rank_and_class","pop_and_class"]
 for i in dellist:
   sub.remove(i)
 sub.insert(0,"Horse")
