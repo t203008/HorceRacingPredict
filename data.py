9e8a9d5879eca511cdf3df27604414a100c8f92b
t203008 <91816208+t203008@users.noreply.github.com>
2023-04-09 13:20:10 +0900

    Update data.py

--- data.py
+++ data.py
@@ -122,8 +122,8 @@ if pred is not None:
   for i in sub:
     if i not in ["Horse","Race","Frame"]:
       predic[i]=predic[i].astype(float,errors="raise")
-  st.dataframe(predic)
   predic=pd.get_dummies(predic,columns=['Frame'])
+  st.dataframe(predic)
   predic["rank_and_class"]=predic["P_rank"]*predic["P_class_Class"]
   predic["pop_and_class"]=predic["P_popular"]*predic["P_class_Class"]
   #predic["pop_rank_class"]=predic["P_popular"]*predic["P_class_Class"]*predic["P_rank"]
