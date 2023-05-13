3abcdbac47b34096b0b003562bbacf99f64b7736
t203008 <91816208+t203008@users.noreply.github.com>
2023-05-13 19:01:36 +0900

    Update data.py

--- data.py
+++ data.py
@@ -21,11 +21,11 @@ st.sidebar.markdown(f"{href}", unsafe_allow_html=True)
 st.sidebar.markdown("入力例")
 image=Image.open("スクリーンショット (506).png")
 st.sidebar.image(image,caption="サイレンススズカの天皇賞(秋)に出る際の確率を調べたいとき",use_column_width=True)
-st.sidebar.markdown("2023年NHKマイルカップ　データ")
-sample2=pd.read_csv("NHK Mile Cup 2023.csv")
-sample2=sample2.to_csv(index=False)  
+st.sidebar.markdown("2023年ビクトリアマイル　データ")
+sample2=pd.read_csv("Victoria Mile 2023.csv")
+sample2=sample2.to_csv(index=False) 
 b64 = base64.b64encode(sample2.encode('utf-8-sig')).decode()
-href = f'<a href="data:application/octet-stream;base64,{b64}" download="NHK Mile Cup 2023.csv">NHK Mile Cup 2023</a>'
+href = f'<a href="data:application/octet-stream;base64,{b64}" download="Victoria Mile 2023.csv">Victoria Mile 2023</a>'
 st.sidebar.markdown(f"{href}", unsafe_allow_html=True)
 
 horse_all=pd.read_csv("Horse_Race.csv")
